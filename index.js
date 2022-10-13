9800af3fde964cc94696740b8d4daa0d81236f8d
Huy nguyen <cushin2302@gmail.com>
2022-10-13 18:23:15 +0700

    Init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    const a = 1;
+    const b = 2;
+
+    const c = a + b;
+    res.send('Hello World!')
+})
+
+app.get('/trangchu', (req, res) => {
+  res.send('Hello World! This is trangchu')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
